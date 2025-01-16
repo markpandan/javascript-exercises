d723cf7b5b49054ca0faa6f5f536a5f80f1b418e
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-16 10:25:13 +0800

    Answer 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let reversedWord = "";
+  for (i = 1; i <= word.length; i++) {
+    reversedWord += word.at(i * -1);
+  }
 
+  return reversedWord;
 };
 
 // Do not edit below this line
