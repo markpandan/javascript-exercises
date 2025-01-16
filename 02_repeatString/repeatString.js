7079a4d0c15d058770e0749b2a0ec302570bc96e
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-16 10:14:40 +0800

    Answer 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
 
+  let repeatedString = "";
+  for (i = 0; i < times; i++) {
+    repeatedString += word;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
