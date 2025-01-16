7702655095d93876a81f8b9d521dcfd68c289190
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-16 11:33:56 +0800

    Answer 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 == 0) {
+    // Years divisible by 100 are not considered as leap years but only for years divisible by 400
+    if (year % 400 == 0) return true;
+    else return false;
+  } else if (year % 4 == 0)
+    // Years divisible by 4 are considered leap years
+    return true;
+  else return false;
 };
 
 // Do not edit below this line
