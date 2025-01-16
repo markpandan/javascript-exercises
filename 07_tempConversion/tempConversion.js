a8b48f09dcfef0eb11566f629f36c4060d461b25
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-16 11:43:41 +0800

    Answer 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+// Formula on converting Fahrenheit to Celsius: x °F ≘ (x − 32) × ⁠5/9 °C
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// Formula on converting Celsius to Fahrenheit: x °C ≘ (x × ⁠9/5⁠ + 32) °F
+const convertToFahrenheit = function (celsius) {
+  return Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
