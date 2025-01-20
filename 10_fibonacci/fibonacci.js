5fe252df65e55f3942dfdc2816654ad1ee7ab784
markpandan <72681401+markpandan@users.noreply.github.com>
2025-01-20 15:56:22 +0800

    Answer 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function (n) {};
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+
+  let n_1 = 1;
+  let n_2 = 0;
+  let result;
+
+  for (i = 2; i <= n; i++) {
+    result = n_1 + n_2;
+
+    n_2 = n_1;
+    n_1 = result;
+  }
+  console.log(result);
+  return result;
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
